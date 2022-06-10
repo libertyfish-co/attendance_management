48db265211bac5955692056366e4af3ff5fd3651
ShimonakaRina-liberty <r_shimonaka@libertyfish.co.jp>
2022-06-10 10:40:08 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :attendances
   devise_for :employees
-  get '/attendances/:yyyymm' => 'attendances#month', as: 'month'
+  get '/attendances/:id/:yyyymm' => 'attendances#month', as: 'month'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
