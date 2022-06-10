9298f183911efebadd91fb1e88de61f8fe62c294
m-ueueda <m_ueda@libertyfish.co.jp>
2022-06-10 12:04:37 +0900

    weekのrouteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :attendances do
     collection do
       get :month
+      get :week
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
