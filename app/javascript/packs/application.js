f0a12a9377ff51949553a9114694f435da359b2f
ShimonakaRina-liberty <r_shimonaka@libertyfish.co.jp>
2022-06-15 11:11:39 +0900

    jQuery使用の準備(month.jsは自分が使う用です。)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// jquery使用のため追記
+require('jquery')
+require('month.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
