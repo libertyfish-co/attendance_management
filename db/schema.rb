1a4f6435236d3c3b59ddf02b36f35b5507894ee5
m-ueueda <m_ueda@libertyfish.co.jp>
2022-06-27 14:18:23 +0900

    db:migrate
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -114,6 +114,8 @@ ActiveRecord::Schema.define(version: 2022_06_20_040614) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "department_id", null: false
     t.index ["department_id"], name: "index_employees_on_department_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
@@ -180,7 +182,6 @@ ActiveRecord::Schema.define(version: 2022_06_20_040614) do
   add_foreign_key "attendances", "employees"
   add_foreign_key "business_calendars", "corporations"
   add_foreign_key "departments", "corporations"
-  add_foreign_key "employees", "departments"
   add_foreign_key "orders", "corporations"
   add_foreign_key "pattern_details", "orders"
   add_foreign_key "pattern_details", "patterns"
