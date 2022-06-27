fa7fe70812f4572c2a6c7bc1790228a87125d4b4
ice-crypto <testtest2tom@gmail.com>
2022-06-27 17:32:02 +0900

    add 全体の枠組み（スケルトン）作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_to
+    # 引数： (string)ym|ymd|ymd/wday|ymw|wday|day|am_pm, (Time)dt
+    # 返り値：文字列（日付）
+
+    # 年月,年月日,年月日曜日,年月週曜日,日,AM/PM
+    # にフォーマットする。
+  end
 end
