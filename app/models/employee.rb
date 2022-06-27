af8727773b63d1484d187bb12350c4b69ec4adf1
= <=>
2022-06-27 10:17:32 +0900

    update employeesのmodelとcontrollerの変更

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,4 +7,19 @@ class Employee < ApplicationRecord
   belongs_to :department
   delegate :corporation, to: :department
   has_many :attendances
+
+  # 物理削除の代わりにユーザーの`deleted_at`をタイムスタンプで更新
+  def soft_delete  
+    update_attribute(:invalid_flag, true)  
+  end
+
+  # ユーザーのアカウントが有効であることを確認 
+  def active_for_authentication?  
+    super && invalid_flag  
+  end  
+
+  # 削除したユーザーにカスタムメッセージを追加します  
+  def inactive_message   
+    invalid_flag ? super : :deleted_account  
+  end 
 end
