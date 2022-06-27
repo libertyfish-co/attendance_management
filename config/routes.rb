c419a6e9127a0a38da3188467164a1ca38a1795c
ice-crypto <testtest2tom@gmail.com>
2022-06-27 13:33:11 +0900

    add routesファイル
    ルートファイルに、パターンとシールズ（印鑑）のルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
       get :week
     end
   end
+  resources :patterns
+  resources :seals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
