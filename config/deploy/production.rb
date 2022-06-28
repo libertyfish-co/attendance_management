417e2930036c245451aed4a0206c3ffb8c7b54be
anjukomaki <a_komaki@libertyfish.co.jp>
2022-06-28 14:05:38 +0900

    fix capistranoの設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{49.212.167.121}
-role :web, %w{49.212.167.121}
-role :db,  %w{49.212.167.121}
+# role :app, %w{49.212.167.121}
+# role :web, %w{49.212.167.121}
+# role :db,  %w{49.212.167.121}
 
-set :password, ask('Server password', nil)
-server '49.212.167.121', user: 'attendance_management', password: fetch(:password), roles: %w{web app db}
+# set :password, ask('Server password', nil)
+# server '49.212.167.121', user: 'attendance_management', password: fetch(:password), roles: %w{web app db}
